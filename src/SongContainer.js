d4a5ee0e6e2e22c3febd6a118fc8887f5270ff30
Cory Trast <cory_trast@hotmail.com>
2020-12-08 15:26:20 -0600

    updated with UI form, post  and delete

--- src/SongContainer.js
+++ src/SongContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Grid } from 'semantic-ui-react';
 import axios from "axios";
-import SongList from './SongList'
+import SongList from "./SongList";
+import CreateSongForm from "./CreateSongForm"
 
 class SongContainer extends Component {
   constructor(props) {
@@ -13,6 +15,31 @@ class SongContainer extends Component {
   componentDidMount() {
     this.getSongs();
   }
+
+  addSong = async (e, song) => {
+    e.preventDefault();
+    console.log(song);
+
+    try {
+      // The createdSongResponse variable will store the response from the Flask API
+      const createdSongResponse = await axios.post(
+        process.env.REACT_APP_FLASK_API_URL + "/api/v1/songs/",
+        song
+      );
+
+      // we are emptying all the songss that are living in state into a new array,
+      // and then adding the song we just created to the end of it
+      // the new song which is called parsedResponse.data
+
+      console.log(createdSongResponse.data.data, " this is response");
+      this.setState({
+        songs: [...this.state.songs, createdSongResponse.data.data],
+      });
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
   getSongs = async () => {
     try {
       const parsedSongs = await axios(
@@ -27,9 +54,31 @@ class SongContainer extends Component {
     }
   };
 
+  deleteSong = async (id) => {
+    console.log(id);
+    const deleteSongResponse = await axios.delete(
+      `${process.env.REACT_APP_FLASK_API_URL}/api/v1/songs/${id}`
+    );
+    console.log(deleteSongResponse);
+    // Now that the db has deleted our item, we need to remove it from state
+    // Then make the delete request, then remove the song from the state array using filter
+    this.setState({ songs: this.state.songs.filter((song) => song.id !== id) });
+
+    console.log(deleteSongResponse, ' response from Flask server');
+  };
+
   render(){
     return (
-      <SongList songs={this.state.songs} />
+      <Grid columns={2} divided textAlign='center' style={{ height: '100%' }} verticalAlign='top' stackable>
+        <Grid.Row>
+          <Grid.Column>
+            <SongList songs={this.state.songs} deleteSong={this.deleteSong}/>
+          </Grid.Column>
+          <Grid.Column>
+           <CreateSongForm addSong={this.addSong}/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
       )
   }
 }
--- src/SongList.js
+++ src/SongList.js
@@ -1,17 +1,18 @@
-import React from "react";
-import { Card, Button } from "semantic-ui-react";
+import React from 'react';
+import { Card, Button } from 'semantic-ui-react';
 
 function SongList(props) {
+  console.log(props);
   const songs = props.songs.map((song) => {
     return (
       <Card key={song.id}>
         <Card.Content>
-          <Card.Header> Title: {song.title}</Card.Header>
-          <Card.Description>Song:{song.artist}</Card.Description>
+          <Card.Header>Title:{song.title}</Card.Header>
+          <Card.Description> Artist:{song.artist}</Card.Description>
           <Card.Description>Album: {song.album}</Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Button>Delete Song</Button>
+          <Button onClick={() => props.deleteSong(song.id)}>Delete Song</Button>
           <Button>Edit Song</Button>
         </Card.Content>
       </Card>
